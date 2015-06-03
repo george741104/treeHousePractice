143d40dabd34461ee91fa99f6842ea9e2546159c
George Chang <georgec1104@gmail.com>
2015-06-03 23:30:13 +0800

    First commit after copying down the project code from Treehouse and edit from MBP

---
+++ project_start/js/app.js
@@ -0,0 +1,17 @@
+// Problem: user interaction doesn't provide the desired results
+// Solution: Add interactivity so the user can manage daily tasks
+
+
+// Add a new task
+    // when the button is clicked
+
+// edit an existing task
+
+
+// Delete an existing tasks
+
+
+// mark a task as complete
+
+
+// mark a task as incomplete
